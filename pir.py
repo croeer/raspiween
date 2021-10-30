137cc126a243473047eb81cbc0e9312c7658f817
croeer <christian@roeer.info>
2021-10-30 20:05:04 +0100

    use threading

--- pir.py
+++ pir.py
@@ -5,6 +5,7 @@ import json
 import os
 import random
 #from playsound import playsound
+import threading
  
 SENSOR_PIN = 23
  
@@ -20,26 +21,27 @@ r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.
 effect_time = 10 # time in 100*ms
 
 def fire_effect():
-    no_loops = random.randint(5,8)
-    for i in range(1,no_loops):
-        hue = random.randint(1,6000)
-        bri = random.randint(10,254)
-        print(hue, bri)
+	no_loops = random.randint(5,8)
+	for i in range(1,no_loops):
+		hue = random.randint(1,6000)
+		bri = random.randint(10,254)
+		#print(hue, bri)
 
-        payload = {"on":True,"bri": bri,"sat": 254,"hue": hue,"transitiontime":effect_time}
-        r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.dumps(payload), headers=headers)
-        print(r.content)
-        time.sleep(effect_time * 1.1 / 10)
+		payload = {"on":True,"bri": bri,"sat": 254,"hue": hue,"transitiontime":effect_time}
+		r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.dumps(payload), headers=headers)
+		#print(r.content)
+		time.sleep(effect_time * 1.1 / 10)
 
-    payload = {"on":False,"transitiontime":effect_time}
-    r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.dumps(payload), headers=headers)
+	payload = {"on":False,"transitiontime":effect_time}
+	r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.dumps(payload), headers=headers)
 
 
 def mein_callback(channel):
 	soundFile = random.randrange(1,5)
 	print('Es gab eine Bewegung! Spiele Sound ' + str(soundFile))
 	#playsound('sounds/sound' + str(soundFile) + '.wav')
-    fire_effect()
+	fire_thread = threading.Thread(target=fire_effect, name="Fire Effect Thread")
+	fire_thread.start()
 	soundfile = 'sounds/sound' + str(soundFile) + '.wav'
 	os.system(' aplay -D bluealsa:DEV=00:1D:DF:6E:F5:C3,PROFILE=a2dp ' + soundfile)
  
