6c574d8202eab3494d233a6b8da63a09fab8fdb4
Röer, Christian <CRoeer@zeb.de>
2021-10-30 20:11:46 +0200

    add fire effect

--- pir.py
+++ pir.py
@@ -17,11 +17,29 @@ hue_ip = "192.168.178.33"
 payload = {"on":False}
 headers = {'content-type': 'application/json'}
 r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.dumps(payload), headers=headers)
+effect_time = 10 # time in 100*ms
+
+def fire_effect():
+    no_loops = random.randint(5,8)
+    for i in range(1,no_loops):
+        hue = random.randint(1,6000)
+        bri = random.randint(10,254)
+        print(hue, bri)
+
+        payload = {"on":True,"bri": bri,"sat": 254,"hue": hue,"transitiontime":effect_time}
+        r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.dumps(payload), headers=headers)
+        print(r.content)
+        time.sleep(effect_time * 1.1 / 10)
+
+    payload = {"on":False,"transitiontime":effect_time}
+    r = requests.put("http://"+hue_ip+"/api/"+hue_user+"/lights/1/state", data=json.dumps(payload), headers=headers)
+
 
 def mein_callback(channel):
 	soundFile = random.randrange(1,5)
 	print('Es gab eine Bewegung! Spiele Sound ' + str(soundFile))
 	#playsound('sounds/sound' + str(soundFile) + '.wav')
+    fire_effect()
 	soundfile = 'sounds/sound' + str(soundFile) + '.wav'
 	os.system(' aplay -D bluealsa:DEV=00:1D:DF:6E:F5:C3,PROFILE=a2dp ' + soundfile)
  
